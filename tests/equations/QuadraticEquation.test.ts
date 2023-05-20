847727d8cb97536388ca02bc61bb9e21200ee3e7
Jorge Barbosa <jorge2007jfsb@gmail.com>
2023-05-19 23:31:39 -0300

    Basics Tests for QuadraticEquation.ts added

---
+++ tests/equations/QuadraticEquation.test.ts
@@ -0,0 +1,12 @@
+import QuadraticEquation from "../../src/equations/quadraticEquation/QuadraticEquation";
+
+
+test('Teste da equação quadrática em que o discriminante é zero', () => {
+    const equation = 'x2 -4x +4 =0';
+    expect((new QuadraticEquation(equation)).execute()).toEqual(["2"])
+});
+
+test('Teste da equação quadrática em que o discriminante é um inteiro real', () => {
+    const equation = '2x^2 + 5x - 3 = 0';
+    expect((new QuadraticEquation(equation)).execute()).toEqual(["1/2", "-3ç"])
+});
