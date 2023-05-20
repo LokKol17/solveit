6cdd4643420c929186b40aa2c69e2908cc7ed9eb
Jorge Barbosa <jorge2007jfsb@gmail.com>
2023-05-20 20:45:22 -0300

    fixed QuadraticEquation.test.ts

--- tests/equations/QuadraticEquation.test.ts
+++ tests/equations/QuadraticEquation.test.ts
@@ -8,5 +8,5 @@ test('Teste da equação quadrática em que o discriminante é zero', () => {
 
 test('Teste da equação quadrática em que o discriminante é um inteiro real', () => {
     const equation = '2x^2 + 5x - 3 = 0';
-    expect((new QuadraticEquation(equation)).execute()).toEqual(["1/2", "-3ç"])
+    expect((new QuadraticEquation(equation)).execute()).toEqual(["1/2", "-3"])
 });
