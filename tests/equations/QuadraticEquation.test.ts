3c6f4053316799ceaa0b781383682b551e84b732
Jorge Barbosa <jorge2007jfsb@gmail.com>
2023-05-22 19:20:44 -0300

    Added tests for incomplete equations

--- tests/equations/QuadraticEquation.test.ts
+++ tests/equations/QuadraticEquation.test.ts
@@ -10,3 +10,17 @@ test('Teste da equação quadrática em que o discriminante é um inteiro real',
     const equation = '2x^2 + 5x - 3 = 0';
     expect((new QuadraticEquation(equation)).execute()).toEqual(["1/2", "-3"])
 });
+
+test('Teste da equação quadrática contem apenas o coeficiente a', () => {
+    const equation = '-7x2 = 0';
+    expect((new QuadraticEquation(equation)).execute()).toEqual(["0"])
+});
+test('Teste da equação quadrática contem apenas os coeficiente a, b', () => {
+    const equation = '3x^2 + 6x = 0';
+    expect((new QuadraticEquation(equation)).execute()).toEqual(["0", "-2"])
+});
+test('Teste da equação quadrática contem apenas os coeficiente a, c', () => {
+    const equation = 'x^2 - 9 = 0';
+    expect((new QuadraticEquation(equation)).execute()).toEqual(["3", "-3"])
+});
+
